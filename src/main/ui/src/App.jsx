519b72bc5f94b89be5f2443285bb18573187bb41
Pablo Martín Anaquín <anaquinpm@gmail.com>
2022-03-29 13:11:32 -0300

    Front-End: Create develop environment structure

---
+++ src/main/ui/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+ import {FaBeer} from 'react-icons/fa';
+
+function App() {
+
+  return (
+    <div className="App">
+      <h1><FaBeer /> Beer Time - Landing Page <FaBeer /></h1>
+    </div>
+  )
+}
+
+export default App
