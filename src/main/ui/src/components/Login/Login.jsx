e16d2fbf64964ca14feebe86168e1df054f7e798
Luciano-Olmedo <lucianonicolasolmedo@gmail.com>
2022-04-09 15:34:40 -0300

    Login:Agregando Validaciones

--- src/main/ui/src/components/Login/Login.jsx
+++ src/main/ui/src/components/Login/Login.jsx
@@ -1,55 +1,100 @@
-import React,{useState}from 'react';
+import React, { useState } from 'react';
 import { FcGoogle } from "react-icons/fc";
 import './Login.css';
+import styled from 'styled-components'
 
 const Login = () => {
-    
-     const [correo, setCorreo] = useState('');
-      
-        const expresiones = {
-          usuario: /^[a-zA-Z0-9\_\-]{4,16}$/, // Letras, numeros, guion y guion_bajo
-          nombre: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, // Letras y espacios, pueden llevar acentos.
-          password: /^.{4,12}$/, // 4 a 12 digitos.
-          correo: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
-          telefono: /^\d{7,14}$/ // 7 a 14 numeros.
+
+    const [correo, setCorreo] = useState('');
+    const [password, setPasword] = useState('');
+    const [mostrarCartelEmail, setMostrarCartelEmail] = useState('none');
+    const [mostrarCartelPassword, setMostrarCartelPassword] = useState('none');
+
+    const expresiones = {
+        usuario: /^[a-zA-Z0-9\_\-]{4,16}$/, // Letras, numeros, guion y guion_bajo
+        nombre: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, // Letras y espacios, pueden llevar acentos.
+        password: /^.{4,12}$/, // 4 a 12 digitos.
+        correo: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
+        telefono: /^\d{7,14}$/ // 7 a 14 numeros.
+    }
+
+    //validar input correo
+    const validateInput = async () => {
+        if (expresiones.correo.test(correo))
+         {
+            setMostrarCartelEmail('none')
+
+            console.log(correo)
+
+
+        } else {
+            setMostrarCartelEmail('block')
+            setCorreo('')
+            console.log(correo)
         }
-      
-      const validateInput = () => {
-        if (expresiones.correo.test(correo)){
-          console.log("todo OK")
-        }else{
-          console.log("todo mal")
+    }
+
+    //Validar input password
+    const validateInputPassword = () => {
+        if (expresiones.password.test(password)) {
+            setMostrarCartelPassword('none')
+            console.log(correo)
+
+        } else {
+            setMostrarCartelPassword('block')
         }
-      }
-      
+    }
+
+
 
     return (
         <div className='container-login'>
             <div className='container caja-login'>
                 <h1 className='title-login'>Login</h1>
-                <div class="input-group mb-3">
-                    <span class="input-group-text  span-form" id="basic-addon1">Email</span>
-                    <input type="text" class="form-control input-login" placeholder="Escribe aqui" aria-label="Username" aria-describedby="basic-addon1" />
+                <div className="input-group mb-3">
+                    <span className="input-group-text  span-form" id="basic-addon1">Email</span>
+                    <input
+                        type="text"
+                        className="form-control input-login"
+                        placeholder="Escribe aqui"
+                        aria-label="Username"
+                        aria-describedby="basic-addon1"
+                        id="correo"
+                        onChange={e => setCorreo(e.target.value)}
+                        onBlur={validateInput}
+                    />
                 </div>
+                <p className='cartel-error ' style={{ display: mostrarCartelEmail }}>Formato de Email Inválido</p>
 
-                <div class="input-group mb-3">
-                    <span class="input-group-text span-form" id="basic-addon1">Password</span>
-                    <input type="text" class="form-control  input-login" placeholder="Escribe aqui" aria-label="Username" aria-describedby="basic-addon1" />
+                <div className="input-group mb-3">
+                    <span className="input-group-text span-form" id="basic-addon1">Password</span>
+                    <input
+                        type="password"
+                        className="form-control  input-login"
+                        placeholder="Escribe aqui"
+                        aria-label="Username"
+                        aria-describedby="basic-addon1"
+                        id="password"
+                        onChange={e => setPasword(e.target.value)}
+                        onBlur={validateInputPassword}
+                    />
                 </div>
-
+                <p className='cartel-error ' style={{ display: mostrarCartelPassword }}>Formato de Password Inválido</p>
                 <button className='button-enviar'>Enviar</button>
-                <a href="#"className='a-login '>Es la primera vez que ingresas?</a>
-                <div className='botones-login'>                    
-                    <button className='boton-google'><span><FcGoogle/></span>  CONTINUAR CON GOOGLE</button>
+                <a href="#" className='a-login '>Es la primera vez que ingresas?</a>
+                <div className='botones-login'>
+                    <button className='boton-google'><span><FcGoogle /></span>  CONTINUAR CON GOOGLE</button>
                     <button className='boton-crear'>CREAR USUARIO</button>
                 </div>
             </div>
-            
+
 
         </div>
-            
+
 
     );
 }
+
+
 
 export default Login;
