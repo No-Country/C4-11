7bb0fa0cb074a28ecf7d65cdf25273c353aa7bfc
Brian Gómez <briang.2012@hotmail.com>
2022-04-18 23:10:08 -0300

    footer fixed

--- src/main/ui/src/components/Footer/Footer.jsx
+++ src/main/ui/src/components/Footer/Footer.jsx
@@ -3,22 +3,22 @@ import "../Footer/Footer.css"
 export const Footer=()=>{
     return(
         <div className="footer">
-            <div className="container d-flex contenedor-footer"> 
-            <div className="footer-container">
-                <p className="texto-footer col-md-8">Seguinos en nuestras redes!</p>
-            <div className="cajon-redes col-md-5">
-            <a>
-            <img className="icono-redes" src="https://i.postimg.cc/xTGT0r0V/facebook-logo-png.png"></img></a>
-            <a>
-            <img className="icono-redes" src="https://i.postimg.cc/8z1kcy5J/instagram-logo-png.png"></img></a>
-            <a>
-            <img className="icono-redes" src="https://i.postimg.cc/BnMSKxLh/whatsapp-logo-png.png" ></img></a>
-            </div>
-            </div>
-            </div>
-
-
+                <div className="footer-container">
+                    <div className="texto-footer ">
+                        <p >Seguinos en nuestras redes!</p>
+                    </div>
+                    <div className="cajon-redes">
+                        <a>
+                            <img className="icono-redes" src="https://i.postimg.cc/xTGT0r0V/facebook-logo-png.png"></img>
+                        </a>
+                        <a>
+                            <img className="icono-redes" src="https://i.postimg.cc/8z1kcy5J/instagram-logo-png.png"></img>
+                        </a>
+                        <a>
+                            <img className="icono-redes" src="https://i.postimg.cc/BnMSKxLh/whatsapp-logo-png.png" ></img>
+                        </a>
+                    </div>
+                </div>
         </div>
-
     )
 }
